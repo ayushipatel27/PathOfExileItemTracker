ff49f704c2e79329954737329acd771ff8513c88
Ayushi <ayushi27@uab.edu>
2017-07-27 16:39:00 -0500

    Added dbfunctions.py, dbconfig.txt, and sprocs

--- app.py
+++ app.py
@@ -6,117 +6,100 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
+from dbfunctions import *
  
 app = Flask(__name__)
 
-# Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'pwd12345'
-app.config['MYSQL_DB'] = 'users'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# init MYSQL
-mysql = MySQL(app)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('home.html')
+	return render_template('home.html')
 
 class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
+	name = StringField('Name', [validators.Length(min=1, max=50)])
+	username = StringField('Username', [validators.Length(min=4, max=25)])
+	email = StringField('Email', [validators.Length(min=6, max=50)])
+	password = PasswordField('Password', [
+		validators.DataRequired(),
+		validators.EqualTo('confirm', message='Passwords do not match')
+	])
+	confirm = PasswordField('Confirm Password')
 
 
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
+	form = RegisterForm(request.form)
+	if request.method == 'POST' and form.validate():
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
 
-        # Create cursor
-        cur = mysql.connection.cursor()
+		credentials = { 'name' : name,
+						'email' : email,
+						'username' : username,
+						'password' : password
+					}
+					
+		createUser(**credentials)
 
-        # Execute query
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+		flash('You are now registered and can log in', 'success')
 
-        # Commit to DB
-        mysql.connection.commit()
-
-        # Close connection
-        cur.close()
-
-        flash('You are now registered and can log in', 'success')
-
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+		return redirect(url_for('login'))
+	return render_template('register.html', form=form)
 
 # User login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # Get Form Fields
-        username = request.form['username']
-        password_candidate = request.form['password']
+	if request.method == 'POST':
+		# Get Form Fields
+		un = request.form['username']
+		password_candidate = request.form['password']
 
-        # Create cursor
-        cur = mysql.connection.cursor()
+		loginInfo = {
+		'username' : un,
+		'password' : password_candidate
+		}
 
-        # Get user by username
-        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+		# Get user by username
+		user = getUser(**loginInfo)
+		username, password = getUser(**loginInfo)
 
-        if result > 0:
-            # Get stored hash
-            data = cur.fetchone()
-            password = data['password']
+		# Compare Passwords
+		if sha256_crypt.verify(password_candidate, password):
+			# Passed
+			session['logged_in'] = True
+			session['username'] = username
 
-            # Compare Passwords
-            if sha256_crypt.verify(password_candidate, password):
-                # Passed
-                session['logged_in'] = True
-                session['username'] = username
+			flash('You are now logged in', 'success')
+			return redirect(url_for('dashboard'))
+		else:
+			error = 'Invalid login'
+			return render_template('login.html', error=error)
 
-                flash('You are now logged in', 'success')
-                return redirect(url_for('dashboard'))
-            else:
-                error = 'Invalid login'
-                return render_template('login.html', error=error)
-            # Close connection
-            cur.close()
-        else:
-            error = 'Username not found'
-            return render_template('login.html', error=error)
 
-    return render_template('login.html')
+	return render_template('login.html')
 
 # Check if user logged in
 def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, Please login', 'danger')
-            return redirect(url_for('login'))
-    return wrap
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('Unauthorized, Please login', 'danger')
+			return redirect(url_for('login'))
+	return wrap
 
 # Logout
 @app.route('/logout')
 @is_logged_in
 def logout():
-    session.clear()
-    flash('You are now logged out', 'success')
-    return redirect(url_for('login'))
+	session.clear()
+	flash('You are now logged out', 'success')
+	return redirect(url_for('login'))
 
 @app.route('/dashboard')
 @is_logged_in
